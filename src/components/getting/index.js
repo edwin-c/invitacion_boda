01c4b9507e74e005304a3c278af81833383e87ae
Jose Daniel Chocoy <danielchocoycordon@hotmail.es>
2024-10-07 23:01:10 -0600

    imagens y botones

--- src/components/getting/index.js
+++ src/components/getting/index.js
@@ -13,8 +13,10 @@ const Getting = (props) => {
     } = props;
 
     const [modal, setModal] = useState(false);
+    const [modal2, setModal2] = useState(false);
 
     const toggle = () => setModal(!modal);
+    const toggle2 = () => setModal2(!modal2);
 
     return (
         <div id="location" className="gta-area section-padding">
@@ -23,9 +25,22 @@ const Getting = (props) => {
                 <div className="row">
                     <div className="col col-lg-8 offset-lg-2 col-md-8 offset-md-2">
                         <div className='row'>
-                            <Button className="btn" onClick={toggle}>Ver mapa</Button>
+                            <div className="w-100 d-flex justify-content-center  px-4 align-items-center pb-3" style={{ gap: '20px' }}>
+                                <Button className="btn " onClick={toggle}>Hotel</Button>
+                                <Button className="btn" onClick={toggle2}>Embarcación</Button></div>
                             <Modal isOpen={modal} toggle={toggle} className={className}>
                                 <ModalHeader toggle={toggle}>Ubicación</ModalHeader>
+                                <ModalBody>
+                                    <div className="location-map">
+                                        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.312332761293!2d-91.28279312507411!3d14.694921085802727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858eb5d6ece6e21b%3A0xe6d8f6e41a0dafdd!2sEco%20Hotel%20Uxlabil%20Atitl%C3%A1n!5e0!3m2!1ses-419!2sgt!4v1727289295001!5m2!1ses-419!2sgt"></iframe> */}
+                                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d722.8062259366787!2d-91.28230813367159!3d14.694357842118546!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858eb5440392b95f%3A0xc4ec5a90eb523390!2sParqueo%20Eco%20Hotel%20Uxlabil%20Atitl%C3%A1n!5e0!3m2!1ses-419!2sgt!4v1727826623298!5m2!1ses-419!2sgt"></iframe>
+                                        {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d57763.58882182253!2d55.38442113562169!3d25.195692423227655!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43496ad9c645%3A0xbde66e5084295162!2z4Kam4KeB4Kas4Ka-4KaHIC0g4Kam4KeB4Kas4Ka-4KaHIOCmhuCmruCmv-CmsOCmvuCmpCAtIOCmuOCmguCmr-CngeCmleCnjeCmpCDgpobgprDgpqwg4KaG4Kau4Ka_4Kaw4Ka-4Kak!5e0!3m2!1sbn!2sbd!4v1540725271741" /> */}
+                                    </div>
+                                </ModalBody>
+                            </Modal>
+
+                            <Modal isOpen={modal2} toggle={toggle2} className={className}>
+                                <ModalHeader toggle={toggle2}>Ubicación</ModalHeader>
                                 <ModalBody>
                                     <div className="location-map">
                                         {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3859.312332761293!2d-91.28279312507411!3d14.694921085802727!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858eb5d6ece6e21b%3A0xe6d8f6e41a0dafdd!2sEco%20Hotel%20Uxlabil%20Atitl%C3%A1n!5e0!3m2!1ses-419!2sgt!4v1727289295001!5m2!1ses-419!2sgt"></iframe> */}
