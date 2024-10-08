316c34692b2fa78841e0faa63f12f7df48bc1d9e
Edwin Chocoy <edwincordon@outlook.es>
2024-10-08 11:16:51 -0600

    Updates

---
+++ precache-manifest.0fbd9096ecaff36fba1dd996c059a915.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e5572ff8b6274a55b1aaf96d416eac",
+    "url": "/invitacion_boda/index.html"
+  },
+  {
+    "revision": "abe2262bf0617cae72c4",
+    "url": "/invitacion_boda/static/css/2.b2e20ed0.chunk.css"
+  },
+  {
+    "revision": "047edaf1d4b8b1a56821",
+    "url": "/invitacion_boda/static/css/main.081db162.chunk.css"
+  },
+  {
+    "revision": "abe2262bf0617cae72c4",
+    "url": "/invitacion_boda/static/js/2.3c5cf998.chunk.js"
+  },
+  {
+    "revision": "047edaf1d4b8b1a56821",
+    "url": "/invitacion_boda/static/js/main.2e42b79f.chunk.js"
+  },
+  {
+    "revision": "d4307e61c0f213cb200c",
+    "url": "/invitacion_boda/static/js/runtime~main.58937cc8.js"
+  },
+  {
+    "revision": "2a4aa695cf176948b28ae92213eb2e5e",
+    "url": "/invitacion_boda/static/media/1.2a4aa695.jpg"
+  },
+  {
+    "revision": "2a4aa695cf176948b28ae92213eb2e5e",
+    "url": "/invitacion_boda/static/media/2.2a4aa695.jpg"
+  },
+  {
+    "revision": "2a4aa695cf176948b28ae92213eb2e5e",
+    "url": "/invitacion_boda/static/media/3.2a4aa695.jpg"
+  },
+  {
+    "revision": "2a4aa695cf176948b28ae92213eb2e5e",
+    "url": "/invitacion_boda/static/media/4.2a4aa695.jpg"
+  },
+  {
+    "revision": "39020e76554b5c366f91bd4810d22aa9",
+    "url": "/invitacion_boda/static/media/5star.39020e76.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/invitacion_boda/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/invitacion_boda/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/invitacion_boda/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/invitacion_boda/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/invitacion_boda/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "41a7043734602d133f7632708716a2fa",
+    "url": "/invitacion_boda/static/media/img-1.41a70437.jpg"
+  },
+  {
+    "revision": "7fb38c9237b81988c99c3a885fc95243",
+    "url": "/invitacion_boda/static/media/img-1.7fb38c92.jpg"
+  },
+  {
+    "revision": "9f41601f7ae4ea9141a6263560fad851",
+    "url": "/invitacion_boda/static/media/img-2.9f41601f.jpg"
+  },
+  {
+    "revision": "ab16590640809c7ee88bb9c5c680e7fd",
+    "url": "/invitacion_boda/static/media/invi-01.ab165906.jpg"
+  },
+  {
+    "revision": "93e1cc7e0bb8ac85ccb14c70adb2f530",
+    "url": "/invitacion_boda/static/media/invi-02.93e1cc7e.jpg"
+  },
+  {
+    "revision": "54f3aeb096e67bda7479821aebd45a0c",
+    "url": "/invitacion_boda/static/media/round_shape.54f3aeb0.svg"
+  },
+  {
+    "revision": "54f3aeb096e67bda7479821aebd45a0c",
+    "url": "/invitacion_boda/static/media/shape1.54f3aeb0.svg"
+  },
+  {
+    "revision": "72d0282b0042ee26fe4ebbd7bfc7dd6c",
+    "url": "/invitacion_boda/static/media/shape2.72d0282b.svg"
+  },
+  {
+    "revision": "173408f151891ca2e00dea9e1055355f",
+    "url": "/invitacion_boda/static/media/shape3.173408f1.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/invitacion_boda/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/invitacion_boda/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/invitacion_boda/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/invitacion_boda/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "3697e1f3ca947845d4487fed877aa3d4",
+    "url": "/invitacion_boda/static/media/star.3697e1f3.svg"
+  },
+  {
+    "revision": "92e75b766c2e05af82292e8b067c094b",
+    "url": "/invitacion_boda/static/media/star_stock.92e75b76.svg"
+  },
+  {
+    "revision": "52e208c85a17e6877630e684994e53fe",
+    "url": "/invitacion_boda/static/media/stock_shape.52e208c8.svg"
+  },
+  {
+    "revision": "265f64ef84d26c41c7b4d17aa57864dd",
+    "url": "/invitacion_boda/static/media/vector.265f64ef.png"
+  },
+  {
+    "revision": "fa68b101dddfa94cea4a762ce996d954",
+    "url": "/invitacion_boda/static/media/vector2.fa68b101.png"
+  }
+]);
